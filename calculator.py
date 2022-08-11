cb4cead07fbe7dbf7a08a963f2f16bf7df5b8e2f
Mudeef <96822029+Mudeef@users.noreply.github.com>
2022-08-11 15:31:23 +0530

    updated

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+replace1=""
+flag="print"
 print("*********  Welcome to the Zodiac calculator")
 print("Enter the number1 :")
 num1=int(input())
@@ -10,21 +12,30 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 print("5.modulus")
+result=0
 operator=input("Please choose an optional from these (1,2,3,4,5):")
 print(operator)
 if operator=="1":
-    print("This is an Additional operation")
-    print("This sum of the two number is :",num1+num2)
+    replace1="Addition"
+    result=num1+num2
 if operator=="2":
-    print("This is an Subtraction operation")
-    print("This difference of the two number is :", num1 - num2)
+
+    if num1<num2:
+        flag="Do not print"
+        print("Cannot subtract the First number is less then the Second number")
+    else:
+        flag="print"
+        replace1="Subtraction"
+        result=num1-num2
 if operator=="3":
-    print("This is an Multiplication operation")
-    print("This product of the two number is :", num1 * num2)
+    replace1="Multiplication"
+    result=num1*num2
 if operator=="4":
-    print("This is an Division operation")
-    print("This  of the two number is :", num1 / num2)
+    replace1="Division"
+    result= num1//num2
 if operator=="5":
-    print("This is an Modulus operation")
-    print("This  of the two number is :", num1 % num2)
+    replace1="Modulus"
+    result=num1%num2
+if flag=="Print":
+    print("The result of",replace1,"of",num1,"and",num2,"is",result)
 
