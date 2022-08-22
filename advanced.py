ef3be25062ffa69515cda33f0557867dd9b6b325
Mudeef <96822029+Mudeef@users.noreply.github.com>
2022-08-22 14:17:49 +0530

    Add files via upload

---
+++ advanced.py
@@ -0,0 +1,9 @@
+class Mudeef:
+    attr1= "Mudeef"
+    attr2= "22"
+    def qualification(self):
+        print("My name is",self.attr1)
+        print("My age is",self.attr2)
+student = Mudeef()
+print(student.attr1)
+student.qualification()
